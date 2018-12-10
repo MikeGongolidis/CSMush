d8b321db63ccc168960b25d30045dc0f9e84b107
MikeGongolidis <mikegongolidis@hotmail.gr>
2018-12-10 23:44:55 +0200

    PAOK GERA

--- data.r
+++ data.r
@@ -5,6 +5,7 @@ library(randomForest)
 library(caTools)   #<- For stratified split
 library(rpart.plot)
 
+library(gridExtra)
 train01<-read.csv("mushrooms_v2.csv")
 mushrooms <- train01
 mushrooms$class <- NULL
@@ -14,7 +15,7 @@ print(z)
 
 
 
-set.seed(121) 
+set.seed(12) 
 sample = sample.split(train01$class, SplitRatio = .7)
 x_train = subset(train01, sample == TRUE)
 x_test = subset(train01, sample == FALSE)
@@ -35,36 +36,39 @@ cv.10.folds<-createMultiFolds(y_train,k=10,times=2)
 ctrl.1<-trainControl(method="repeatedcv",number=10,repeats=2,index=cv.10.folds)
 
 #GBM
-#mod2<-train(x=x_train,y=y_train,method="gbm", trControl=ctrl.1,tuneLength=3,verbose=FALSE)
+gbm<-train(x=x_train,y=y_train,method="gbm", trControl=ctrl.1,tuneLength=3,verbose=FALSE)
 
+plot(gbm,main="Gradient Boosting Model ")
+y_predicted<-predict(gbm,x_test)
+
+df1<-data.frame(Orig=y_test,Pred=y_predicted)
+
+confusionMatrix(table(df1$Orig,df1$Pred))
 #RPART
 
-mod3 <-train(x=x_train,y=y_train,method="rpart",trControl=ctrl.1,tuneLength=5)
-#plot(varImp(mod3),main="RPart - Variable Importance Plot")
+rpart <-train(x=x_train,y=y_train,method="rpart",trControl=ctrl.1,tuneLength=5)
+plot(varImp(rpart),main="RPart - Variable Importance Plot")
+plot(rpart,main="RPart")
+rpart.plot(rpart$finalModel) 
+
+y_predicted<-predict(rpart,x_test)
+
+df1<-data.frame(Orig=y_test,Pred=y_predicted)
+
+confusionMatrix(table(df1$Orig,df1$Pred))
 #RANDOM FOREST
 
-rf.1.cv<-train(x=x_train,y=y_train,method="rf",trControl=ctrl.1,tuneLength=3)
-#summary(rf.1.cv) acc =0.8907171
-#plot(varImp(rf.1.cv),main="Random Forest - Variable Importance Plot")
+rf<-train(x=x_train,y=y_train,method="rf",trControl=ctrl.1,tuneLength=3)
+summary(rf) #acc =0.8907171
+plot(varImp(rf),main="Random Forest - Variable Importance Plot")
 
 
 
-#y_predicted<-predict(rf.1.cv,x_test)
+y_predicted<-predict(rf,x_test)
 
-#df1<-data.frame(Orig=y_test,Pred=y_predicted)
+df1<-data.frame(Orig=y_test,Pred=y_predicted)
 
-#confusionMatrix(table(df1$Orig,df1$Pred))  #<-100% accuracy
-
-#RPART
-
-#mod3 <-train(x=x_train,y=y_train,method="rpart",trControl=ctrl.1,tuneLength=5)
-#plot(varImp(mod3),main="RPART - Variable Importance Plot")
-
-#rpart.plot(mod3$finalModel) 
-#install.packages("rattle")
-#library(rattle)
-#fancyRpartPlot(mod3$finalModel,main="Mushroom Attributes Classification",sub=NA,
-         #      palettes=c("Greys","Oranges"))
+confusionMatrix(table(df1$Orig,df1$Pred))  
 
 # TEST HERE OUR WORK
 
@@ -74,3 +78,44 @@ predict(mod3,test_variable)
 #y = data.frame("a","b","f","f","f","f","a","b","f","f","f","f","a","b","f","f","f","f","a","b","f","f")
 #names(y)<- names
 #predict(rf.1.cv,y)
+
+
+###############################
+#  PLOTS                      #
+###############################
+
+m1 <- ggplot(aes(x = cap.shape), data = mushrooms) +
+  geom_histogram(stat = "count") +
+  facet_wrap(~class) +
+  xlab("Cap Shape")
+
+m2 <- ggplot(aes(x = cap.surface), data = mushrooms) +
+  geom_histogram(stat = "count") +
+  facet_wrap(~class) +
+  xlab("Cap Surface")
+
+m3 <- ggplot(aes(x = cap.color), data = mushrooms) +
+  geom_histogram(stat = "count") +
+  facet_wrap(~class) +
+  xlab("Cap Color")
+
+m4 <- ggplot(aes(x = bruises), data = mushrooms) +
+  geom_histogram(stat = "count") +
+  facet_wrap(~class) +
+  xlab("Bruises")
+
+m5 <- ggplot(aes(x = odor), data = mushrooms) +
+  geom_histogram(stat = "count") +
+  facet_wrap(~class) +
+  xlab("Odor")
+
+grid.arrange(m1, m2, m3, ncol = 2)
+grid.arrange(m4,m5,ncol=2)
+
+p = ggplot(mushrooms,aes(x=odor,  
+                    y=spore.print.color, 
+                    color=class))
+
+p + geom_jitter(alpha=0.3) +  
+  scale_color_manual(breaks = c('Edible','Poisonous'),
+                     values=c('darkgreen','red'))
